3eb8d78d50c8c0c21305ae574a87cb4df5675c93
priyanshuprafful <priyanshuprafful@gmail.com>
2025-09-20 18:57:04 +0530

    updating ./tf-module-app-2.0

--- main.tf
+++ main.tf
@@ -6,13 +6,66 @@ terraform {
     }
   }
 }
-resource "null_resource" "test" {
+## policy banana hai
+resource "aws_iam_policy" "policy" {
+  name        = "${var.component}-${var.env}-ssm-parameter-policy"
+  path        = "/"
+  description = "${var.component}-${var.env}-ssm-parameter-policy to fetch the parameters"
 
-  triggers = {
-    xyz = timestamp()
-  }
-  provisioner "local-exec" {
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:275396904943:parameter/roboshop.${var.env}.${var.component}.*"
+      }
+    ]
+  })
+}
 
-    command = "echo Hello All - Env - ${var.env}"
+## Iam role ko attach karna hai policy se
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
   }
 }
+
+
+## security group banana hai
+
+
+##ec2
+
+
+##DNS record route 53 ke liye
+
+##null resource to run ansible
+
