00c4c4badd15c6380c6c3b7dc97a0a1ed33e2d4f
priyanshuprafful <priyanshuprafful@gmail.com>
2025-09-20 20:19:41 +0530

    updating ./tf-module-app-2.0

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.3"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = "6.2.0"
+      version = "~> 5.80"
     }
   }
- }
+}
+
 ## policy banana hai
 resource "aws_iam_policy" "policy1" {
   name        = "${var.component}-${var.env}-ssm-parameter-policy"
