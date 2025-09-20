cb2271a67a6aa541fa2237cf9c227ef3703bfc4b
priyanshuprafful <priyanshuprafful@gmail.com>
2025-09-20 17:18:31 +0530

    updating ./tf-module-app-2.0

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "echo Hello All -${var.env}"
+  }
+}
