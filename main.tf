51b52dc62e5c56582592dac72f56b4d3d35758dd
priyanshuprafful <priyanshuprafful@gmail.com>
2025-09-20 19:00:32 +0530

    updating ./tf-module-app-2.0

--- main.tf
+++ main.tf
@@ -35,27 +35,22 @@ resource "aws_iam_policy" "policy" {
 ## Iam role ko attach karna hai policy se
 
 resource "aws_iam_role" "test_role" {
-  name = "test_role"
+  name = "${var.component}-${var.env}-EC2-role"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
     ]
   })
-
-  tags = {
-    tag-key = "tag-value"
-  }
 }
 
 
