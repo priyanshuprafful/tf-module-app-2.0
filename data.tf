99fb7743c5e42190f7d6ce00db56b1ffc81cf480
priyanshuprafful <priyanshuprafful@gmail.com>
2025-09-30 18:44:57 +0530

    updating ./tf-module-app-2.0

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "centos8" {
 
-  owners = ["973714476881"]
+  owners = ["275396904943"]
   most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
+  name_regex = "centos-8-with-ansible"
 }
